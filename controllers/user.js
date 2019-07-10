1cf01d702184ce3269446dcd5380932bb33aff18
Keven <kevenmolina@gmail.com>
2019-07-10 11:09:49 -0500

    users added to controllers

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+
+
+function createJWT(user){
+    return jwt.sign(
+        {user},
+        SECRET,
+        {expiresIn: ''}
+    )   
+}
