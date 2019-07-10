c22edce35f24871b6e998c510e94f958f6099f5e
Keven <kevenmolina@gmail.com>
2019-07-10 11:11:04 -0500

    added to json, added db

--- src/App.js
+++ src/App.js
@@ -6,18 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      Hello World
+      {/* create login component */}
+      {/* create landing/gallery page component */}
+      {/* create submit page component */}
+      {/* create  page component */}
       </header>
     </div>
   );
