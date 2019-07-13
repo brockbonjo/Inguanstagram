8457b5b70c34d43bd3b4ad71c4132d161941d347
Keven <kevenmolina@gmail.com>
2019-07-13 11:00:47 -0500

    add to signup page, handle change function will need bug fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import userServices from '../utils/userServices'
 
 class Login extends React.Component{
     state = {
@@ -7,29 +8,58 @@ class Login extends React.Component{
         password:''
     }
 
+    handleChange = (e) => {
+        this.setsState({
+            [e.target.name]: e.target.value
+        });
+    }
+
     handleSignupOrLogin = () => {
         this.setState({user: userServices.getUser()})};
 
 
-    render(){
-        return(
-    <div>
-            <header className="header-footer">Sign UP</header>
-                <form className="form-horizontal" onSubmit={this.handleSubmit} >
-                <div className="form-group">
-                    <div className="col-sm-12">
-                        <input type="text" className="form-control" placeholder="Name" value={this.state.username} name="username" onChange={this.handleChange} />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="col-sm-12">
-                        <input type="text" className="form-control" placeholder="Password" value={this.state.username} name="password" onChange={this.handleChange} />
-                    </div>
-                </div>
-            </form>
-    </div>
-            )
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await userServices.login(this.state);
+            // in apps ultimately getsUser, => getTokenfrom User
+            this.props.handleSignupOrLogin();
+            this.props.history.push('/');
+        } catch(err){
+            console.log(err)
+            console.log('Invalid Credentials');
         }
-    };
+    }
+    isFormInvalid(){
+        return !(this.state.name&& this.state.password === this.state.passwowrdConf);
+    }
+
+    render() {
+        return (
+          <div className="LoginPage">
+            <header className="header-footer">Log In</header>
+            <form className="form-horizontal" onSubmit={this.handleSubmit} >
+              <div className="form-group">
+                <div className="col-sm-12">
+                  <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="col-sm-12">
+                  <input type="password" className="form-control" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="col-sm-12 text-center">
+                  <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
+                  <Link to='/'>Cancel</Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        );
+      }
+    }
+    
 
 export default Login
